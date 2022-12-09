f3f4e197c01f87937a9db11cb55faaa9e499b6fe
SpBills <spencebills0@gmail.com>
2022-12-09 17:23:42 -0500

    responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col justify-center ml-20">
+<div class="flex flex-col justify-center ml-5 md:ml-20">
   <h1 class="text-5xl">Spencer Bills</h1>
   <p class="mt-5">Fullstack Software Developer</p>
   <a class="mt-5 underline" href="resume.pdf">Download My Resume</a>
