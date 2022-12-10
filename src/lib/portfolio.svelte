09ed47ec014a553134a6c9e45fb427a18b6cfc7f
spbills <spencebills0@gmail.com>
2022-12-10 13:32:39 -0500

    Added portfolio items

--- src/lib/portfolio.svelte
+++ src/lib/portfolio.svelte
@@ -6,27 +6,32 @@
     title: string;
     description: string;
     url?: string;
-    image_src: string;
+    image_src?: string;
   }
 
   const resume_items: Array<ResumeItem> = [
     {
+      title: "Creative Solutions Group",
+      description: "A team of eight software developers for Cedarville University- of which I am the team lead and tech lead.",
+      image_src: "https://github.com/CreativeSolutionsGroup/.github/raw/main/csg-social-logo.jpg",
+      url: "https://github.com/CreativeSolutionsGroup",
+    },
+    {
       title: "Rapit",
-      description: "A journey in self-education and compiler theory.",
-      image_src: p1,
+      description: "A journey in self education and compiler theory.",
       url: "https://github.com/SpBills/Rapit",
     },
   ];
 </script>
 
-<div class="flex mx-5 md:mx-0 justify-center flex-wrap">
+<div class="flex mx-5 md:mx-0 justify-center w-full md:w-10/12 flex-wrap">
   {#each resume_items as item}
     <div
-      class="rounded overflow-hidden shadow-lg w-full md:w-1/3 h-80 flex flex-col"
+      class="rounded overflow-hidden mx-5 shadow-lg w-full md:w-1/3 pb-5 flex flex-col"
       on:click={() => goto(item.url ?? "")}
       style:cursor={item.url ? "pointer" : ""}
     >
-      <img class="h-52 w-full object-none" src={item.image_src} />
+      <img class="h-52 w-full object-none" src={item.image_src ?? p1} />
       <div class="px-5 pt-3">
         <h2 class="text-3xl font-bold">{item.title}</h2>
         <h3 class="mt-3">{item.description}</h3>
